c2d39917537e3e31191530424c26b01cf9f50a2d
Benjamin Smith <ben.smith.developer@gmail.com>
2025-08-31 10:10:24 -0400

    aws provider

--- src/versions.tf
+++ src/versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9.0, < 6.0.0"
+    }
     datadog = {
       source = "datadog/datadog"
       # Child organizations are only available in v3.3.0 and above
