062977c50c19ef3c2fed0de16d252bda993038bf
Benjamin Smith <ben.smith.developer@gmail.com>
2025-09-02 07:46:44 -0700

    disable advanced testing

--- test/component_test.go
+++ test/component_test.go
@@ -1,12 +1,10 @@
 package test
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/cloudposse/test-helpers/pkg/atmos"
 	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -18,20 +16,12 @@ type ComponentSuite struct {
 func (s *ComponentSuite) TestBasic() {
 	const component = "example/basic"
 	const stack = "default-test"
-	const awsRegion = "us-east-2"
 
-	var randomString = strings.ToLower(random.UniqueId())
-	inputs := map[string]interface{}{
-		"organization_name": randomString,
-		"region":            awsRegion,
-	}
-
-	defer s.DestroyAtmosComponent(s.T(), component, stack, &inputs)
-	componentInstance, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	componentInstance, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
 	require.NotNil(s.T(), componentInstance)
 
 	assert.NotEmpty(s.T(), atmos.Output(s.T(), componentInstance, "id"))
-	assert.Contains(s.T(), atmos.Output(s.T(), componentInstance, "id"), randomString)
 	assert.NotEmpty(s.T(), atmos.Output(s.T(), componentInstance, "description"))
 	assert.NotEmpty(s.T(), atmos.Output(s.T(), componentInstance, "public_id"))
 	assert.NotEmpty(s.T(), atmos.Output(s.T(), componentInstance, "user"))
